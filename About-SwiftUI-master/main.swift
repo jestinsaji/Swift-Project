c9e21178329c2fa6666600f2b757083a7937d986
JestinSaji9977 <93115198+JestinSaji9977@users.noreply.github.com>
2022-10-11 13:07:37 +0530

    Update main.swift

--- About-SwiftUI-master/main.swift
+++ About-SwiftUI-master/main.swift
@@ -1,1 +1,14 @@
 // TO-DO
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.showHideNavigationBar(showNavigationBar: false)
+    }
